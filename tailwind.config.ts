9fc6ce9bd049016b47b8f7b36a31a9c1b811f72b
Lakshmi <lakshmi.chennakesavalu1998@gmail.com>
2025-07-09 13:01:01 +0000

    it still looks like this

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
-        headline: ['"PT Sans"', 'sans-serif'],
+        body: ['"Noto Sans"', 'sans-serif'],
+        headline: ['"Noto Sans"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
